b50fb1ddf81596fbf77484babe5913b6eef5bfbc
Abed Murrar <abedmurrar15@gmail.com>
2018-12-31 16:04:49 +0200

    add functions to course and students controllers

--- src/main/java/com/comp4382/ass3/assignment3/course/CourseController.java
+++ src/main/java/com/comp4382/ass3/assignment3/course/CourseController.java
@@ -1,14 +1,20 @@
 package com.comp4382.ass3.assignment3.course;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.data.domain.Pageable;
 
@@ -22,15 +28,34 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<?> getCourses(Pageable page, @PathVariable(name = "id", required = false) Integer id) {
-        if (id != null) {
-            return Optional.ofNullable(courseService.getCourseById(id)).map(course -> ResponseEntity.ok().body(course))
-                    .orElseGet(() -> ResponseEntity.notFound().build());
-            // return ResponseEntity.ok(courseService.getCourseById(id));
-        }
-        return Optional.ofNullable(courseService.getCourses(page)).map(course -> ResponseEntity.ok().body(course))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/courses")
+    public ArrayList<Course> getAllCourses(@RequestParam(value = "university", required = false) String university,
+            @RequestParam(value = "year", required = false) String year) {
+        ArrayList<Course> courses = new ArrayList<Course>();
+        return courses;
+    }
+
+    @GetMapping("/courses/{id}")
+    public Course getUserById(@PathVariable Integer id) {
+        if (id != null)
+            return new Course();
+        else
+            return null;
+    }
+
+    @PostMapping("/courses")
+    public void addNewCourse(@RequestBody Course course) {
+        //
+    }
+
+    @PutMapping("/courses/{id}")
+    public void editCourse(@RequestBody Course newCourse, @PathVariable int id) {
+        //
+    }
+
+    @DeleteMapping("courses/{id}")
+    public void deleteCourse(@PathVariable int id) {
+        //
     }
 
 }
--- src/main/java/com/comp4382/ass3/assignment3/student/StudentController.java
+++ src/main/java/com/comp4382/ass3/assignment3/student/StudentController.java
@@ -1,10 +1,17 @@
 package com.comp4382.ass3.assignment3.student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,18 +26,32 @@ public class StudentController {
     private StudentRepository studentRepository;
 
     @GetMapping("/students")
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
+    public ArrayList<Student> getAllStudents(@RequestParam(value = "university", required = false) String university,
+            @RequestParam(value = "year", required = false) String year) {
+        ArrayList<Student> students = new ArrayList<Student>();
+        return students;
     }
 
-    // @PostMapping("/students")
-    // public ResponseEntity<Object> createStudent(@RequestBody Student student) {
-    // Student savedStudent = studentRepository.save(student);
+    @GetMapping("/students/{id}")
+    public Student getUserById(@PathVariable Integer id) {
+        if (id != null)
+            return new Student();
+        else
+            return null;
+    }
 
-    // URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-    // .buildAndExpand(savedStudent.getId()).toUri();
+    @PostMapping("/students")
+    public void addNewStudent(@RequestBody Student student) {
+        //
+    }
 
-    // return ResponseEntity.created(location).build();
+    @PutMapping("/students/{id}")
+    public void editStudent(@RequestBody Student newStudent, @PathVariable int id) {
+        //
+    }
 
-    // }
+    @DeleteMapping("students/{id}")
+    public void deleteStudent(@PathVariable int id) {
+        //
+    }
 }
